b51ce18a77f670fb7e704886b7d4a3bf78ea331d
¨Unlockeagle¨ <unlock.eagle@gmail.com>
2023-06-23 16:53:20 -0400

    arrays2

---
+++ src/javascript/metodosString.ts
@@ -0,0 +1,29 @@
+// metodos de arreglos
+
+// metodo push es para empujar un tipo de dato
+// condicional sort element
+
+export default function main(){
+
+    const arreglo: Array<number | boolean | string | Array<string> | object | undefined> [
+        3,
+        1,
+        2,
+        4,
+        'Leonardo',
+        'Instituto',
+        true,
+        flase,
+        true,
+        ['Caracas', 'Deltaamacuro', 'Caracas', 'Bolivar'],
+        ['Venezuela', 'Colombia', 'Brazil', 'Paraguay', 'Chile' ],
+        [nombre: 'Leonardo', edad: 4]
+
+    ]
+
+};
+
+
+
+
+// arreglo.forEach((element) => console.log(element));
